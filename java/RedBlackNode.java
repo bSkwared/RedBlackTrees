7ae040b9fc3b95a54cfe8803e5845450beb09b66
bSkwared <gh@truebearsan.com>
2016-11-08 21:24:30 -0500

    Created files

---
+++ java/RedBlackNode.java
@@ -0,0 +1,14 @@
+public class RedBlackNode<T extends Comparable<T>> {
+    private enum Color {RED, BLACK};
+
+    private RedBlackNode parent;
+    
+    private RedBlackNode<T> left;
+    private RedBlackNode<T> right;
+
+    private T value;
+
+
+    
+
+}
